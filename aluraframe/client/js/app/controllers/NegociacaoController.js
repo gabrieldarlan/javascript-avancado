db7b10e8011c2c809ddf7c0f21db45f67fc7727c
gabrieldarlan <gabriel.darlan88@gmail.com>
2020-03-30 20:02:20 -0300

    capitulo 4 completo

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -19,6 +19,21 @@ class NegociacaoController {
         this._limpaFormulario();
     }
 
+    importaNegociacoes() {
+
+        let service = new NegociacaoService();
+        service.obterNegociacoesDaSemana((erro, negociacoes) => {
+            if (erro) {
+                this._mensagem.texto = erro;
+                return;
+            }
+            negociacoes.forEach(negociacao => {
+                this._listaNegociacoes.adiciona(negociacao);
+            });
+            this._mensagem.texto='Negociações importadas com sucesso'
+        });
+    }
+
     apaga() {
         this._listaNegociacoes.esvazia();
 
